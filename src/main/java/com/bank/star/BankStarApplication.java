e9bb1f543ad45c0bb947d1d0edf7a4216c6bcd52
yagitpa <yakov.palchikov@gmail.com>
2026-01-10 03:46:37 +0700

    Initial commit

---
+++ src/main/java/com/bank/star/BankStarApplication.java
@@ -0,0 +1,13 @@
+package com.bank.star;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankStarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankStarApplication.class, args);
+    }
+
+}
